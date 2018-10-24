7c9e5096db28c5de877c23585e6ea0e59717e003
majorazero <daniel.cw.hsu@gmail.com>
2018-10-23 19:03:58 -0700

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.js";
+import Saved from "./components/Saved/Saved.js";
+import Search from "./components/Search/Search.js";
+import Results from "./components/Results/Results.js";
 import './App.css';
 
 class App extends Component {
@@ -9,6 +12,9 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
+          <Route path="/" component={Search} />
+          <Route path="/" component={Results} />
+          <Route path="/saved" component={Saved} />
         </div>
       </Router>
     );
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,6 +6,9 @@ const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">Navbar</a>
       <div className="navbar-nav">
+        <Link className="nav-item nav-link" to="/">Search</Link>
+        <Link className="nav-item nav-link" to="/results">Results</Link>
+        <Link className="nav-item nav-link" to="/saved">Saved</Link>
       </div>
     </nav>
   );
