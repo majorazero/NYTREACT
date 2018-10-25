d0e6eb6d62c755de79297b3a254342961afe9daf
majorazero <daniel.cw.hsu@gmail.com>
2018-10-24 20:11:23 -0700

    added build

---
+++ client/build/precache-manifest.3e3041665f18887e974d568cc800a0d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efc82f52c7a9064ce81d",
+    "url": "/static/js/main.efc82f52.chunk.js"
+  },
+  {
+    "revision": "96809a54fb15d9936dfe",
+    "url": "/static/js/1.96809a54.chunk.js"
+  },
+  {
+    "revision": "efc82f52c7a9064ce81d",
+    "url": "/static/css/main.e67c9675.chunk.css"
+  },
+  {
+    "revision": "8d92ef707dc283f10aa00ddcba1a3a33",
+    "url": "/index.html"
+  }
+];
