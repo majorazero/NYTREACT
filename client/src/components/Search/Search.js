d1a453c8e25338b22fafac180e18025b8f5ab576
majorazero <daniel.cw.hsu@gmail.com>
2018-10-24 20:00:23 -0700

    removed useless code

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import axios from "axios";
 import Results from "../Results/Results.js";
-import Saved from "../Saved/Saved.js";
 import "./Search.css";
 
 class Search extends Component {
